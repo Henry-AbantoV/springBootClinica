9ba564cb796801157698507d63c1e4600e5adeac
Daniela Torres <dtorresd20_2@unc.edu.pe>
2024-03-12 02:14:55 -0500

    CitaRepository encabezado

--- src/main/java/edu/unc/clinica/dto/PacienteResponseDTO.java
+++ src/main/java/edu/unc/clinica/dto/PacienteResponseDTO.java
@@ -1,14 +1,33 @@
+/*
+ * @file PacienteResponseDTO.java;
+ * @Autor Daniela Torres (c)2024
+ * @Created 12 mar 2024,2:09:31
+ */
 package edu.unc.clinica.dto;
 
 import lombok.Data;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PacienteResponseDTO.
+ */
 @Data
 public class PacienteResponseDTO {
+	
+	/** The nombres paciente. */
 	// Add the fields you want to include in the response
 	private String nombres_paciente;
+	
+	/** The apellidos. */
 	private String apellidos;
 	
 	
+	/**
+	 * Instantiates a new paciente response DTO.
+	 *
+	 * @param nombres_paciente the nombres paciente
+	 * @param apellidos the apellidos
+	 */
 	public PacienteResponseDTO(String nombres_paciente, String apellidos) {
 		super();
 		this.nombres_paciente = nombres_paciente;
--- src/main/java/edu/unc/clinica/repositories/CitaRepository.java
+++ src/main/java/edu/unc/clinica/repositories/CitaRepository.java
@@ -1,9 +1,17 @@
+/*
+ * @file CitaRepository.java;
+ * @Autor Daniela Torres (c)2024
+ * @Created 12 mar 2024,2:09:45
+ */
 package edu.unc.clinica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.unc.clinica.domain.Cita;
 
+/**
+ * The Interface CitaRepository.
+ */
 public interface CitaRepository extends JpaRepository<Cita, Long> {
 
 }
