041c850f0c238aeb87b1153958673ec7102f6e5d
BFernand0 <bchilonj20_1@unc.edu.pe>
2024-03-12 02:01:43 -0500

    Especialidad Respository Documentación

--- src/main/java/edu/unc/clinica/repositories/EspecialidadRepository.java
+++ src/main/java/edu/unc/clinica/repositories/EspecialidadRepository.java
@@ -1,9 +1,17 @@
+/*
+ * @file EspecialidadRepository.java;
+ * @Autor Fernando C.J. (c)2024
+ * @Created 12 mar. 2024,02:00:57
+ */
 package edu.unc.clinica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.unc.clinica.domain.Especialidad;
 
+/**
+ * The Interface EspecialidadRepository.
+ */
 public interface EspecialidadRepository extends JpaRepository<Especialidad, Long> {
 
 }
