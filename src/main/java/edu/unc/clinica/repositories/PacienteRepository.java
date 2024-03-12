a8e2ebf10d7539d153162a4f450fb7cca014f138
BFernand0 <bchilonj20_1@unc.edu.pe>
2024-03-11 19:59:28 -0500

    documentacion paciente repository

--- src/main/java/edu/unc/clinica/repositories/PacienteRepository.java
+++ src/main/java/edu/unc/clinica/repositories/PacienteRepository.java
@@ -7,13 +7,43 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import edu.unc.clinica.domain.Paciente;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    /**
+     * Busca pacientes por nombres.
+     *
+     * @param nombres Nombres del paciente a buscar.
+     * @return Lista de pacientes que coinciden con los nombres proporcionados.
+     */
 	List<Paciente> findByNombres(String nombres);
 
+    /**
+     * Busca pacientes por DNI.
+     *
+     * @param dnipaciente DNI del paciente a buscar.
+     * @return Lista de pacientes que coinciden con el DNI proporcionado.
+     */
 	List<Paciente> findBydni(String dnipaciente);
 
+    /**
+     * Busca pacientes por correo electrónico.
+     *
+     * @param correopaciente Correo electrónico del paciente a buscar.
+     * @return Lista de pacientes que coinciden con el correo electrónico proporcionado.
+     */
 	List<Paciente> findByCorreoElectronico(String correopaciente);
 
+    /**
+     * Busca pacientes por dirección.
+     *
+     * @param direccionpaciente Dirección del paciente a buscar.
+     * @return Lista de pacientes que coinciden con la dirección proporcionada.
+     */
 	List<Paciente> findByDireccion(String direccionpaciente);
 
+    /**
+     * Busca pacientes por número de teléfono.
+     *
+     * @param telefonopaciente Número de teléfono del paciente a buscar.
+     * @return Lista de pacientes que coinciden con el número de teléfono proporcionado.
+     */
 	List<Paciente> findByTelefono(String telefonopaciente);
 }
